47a546134d310f1f3ddd8243e58268d994b09d47
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-08-26 14:36:06 -0700

    Added TODO comment

--- iOS_Project/ViewController.swift
+++ iOS_Project/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //TODO: Remove StoryBoard Files and Create View Programiatically
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
